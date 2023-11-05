65d9a842d7ce2362bb054ffadb5c970592a1a656
michelletaing <mleetaing@gmail.com>
2023-11-04 22:14:38 -0400

    added cards for search results

--- index.js
+++ index.js
@@ -10,6 +10,30 @@ document.getElementById('searchButton').addEventListener('click', async () => {
         
         // Handle and display the data in your extension
         console.log('Received data from the server:', data);
+
+        // Display the response in the "searchResults" div
+        const searchResultsDiv = document.getElementById('searchResults');
+        searchResultsDiv.innerHTML = ''; // Clear any previous content
+
+        // Create and append elements for each result
+        data.results.forEach(result => {
+            // Create an HTML string representing the Bootstrap card
+            const cardHtml = `
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h5 class="card-title">${result.title}</h5>
+                        </div>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">${result.author}</h6>
+                        <p class="card-text">${'This is an example of a summary of a webpage.'}</p>
+                        <a href="${result.url}" class="card-link" target="_blank">Read more</a>
+                    </div>
+                </div>
+            `;
+
+            // Append the card HTML to the "searchResults" div
+            searchResultsDiv.innerHTML += cardHtml;
+        });
     } catch (error) {
         console.error('An error occurred while fetching data:', error);
     }
