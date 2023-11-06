dd10a628a6df08830d9350e3f4c9fe881bef12c5
michelletaing <mleetaing@gmail.com>
2023-11-06 15:08:28 -0500

    capture highlighted text

--- background.js
+++ background.js
@@ -1,12 +1,30 @@
-chrome.contextMenus.create({
-    id: "sift-search",
-    title: "Search with sift!",
-    contexts: ["all"]
-});
+function createContextMenuItem() {
+    chrome.contextMenus.create({
+      id: "sift-search",
+      title: "Search with sift!",
+      contexts: ["all"]
+    })
+}
+
+// Create the context menu item when the extension is loaded
+chrome.runtime.onInstalled.addListener(createContextMenuItem);
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId == "sift-search") {
-        console.log("yay!");
-    }
+    const selectedText = info.selectionText;
+    console.log(selectedText);
+    // chrome.browserAction.openPopup();
+
+    // Send a message to the popup script with the selected text
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const activeTab = tabs[0];
+        chrome.scripting.executeScript({
+            target: { tabId: activeTab.id },
+            function: function (selectedText) {
+                // This function runs in the content script of the active tab
+                chrome.runtime.sendMessage({ action: "showPopup", selectedText });
+            },
+            args: [selectedText],
+        });
+    });
 });
   
